d88479105d38460038045d7da6239a2f8411c685
Rodrigo Quevedo (TalentHub) <rodrigo.quevedo@talenthubai.com.ar>
2025-10-03 19:29:42 -0300

    feature: fichas producto - Tractor Nuevo, Tractor Usado, Repuesto.

--- app/tractores/[id]/page.tsx
+++ app/tractores/[id]/page.tsx
@@ -4,11 +4,11 @@ import CotizarButton from "@/components/SectionCatalogo/buttons/CotizarButton";
 import VolverButton from "@/components/SectionCatalogo/buttons/VolverButton";
 import CardProducto from "@/components/SectionCatalogo/CardProducto";
 import getDemoData from "@/components/SectionCatalogo/utils/getDemoData"
-import { Implemento } from "@/types/Producto";
+import { TractorNuevo } from "@/types/Producto";
 
 
 
-const productos = getDemoData("implementos") as Implemento[];
+const productos = getDemoData("tractores") as TractorNuevo[];
 
 interface Props {
   params: { id: string };
@@ -16,11 +16,11 @@ interface Props {
 
 
 export default function ImplementoPage({ params }: Props){
-    const implemento = productos.find(p=>p.id === Number(params.id)) as Implemento;
+    const tractor = productos.find(p=>p.id === Number(params.id)) as TractorNuevo;
 
-    const recomendados = productos.filter(p=>p.categoria===implemento?.categoria).slice(0, 10);
+    const recomendados = productos.filter(p=>p.marca===tractor?.marca).slice(0, 10);
 
-    const urlCatalogo = "/implementos"
+    const urlCatalogo = "/tractores";
 
     return (
     <main className="min-h-screen bg-gray-50 pt-10">
@@ -29,16 +29,16 @@ export default function ImplementoPage({ params }: Props){
             {/* Volver */}
             <div className="col-span-2 flex justify-between items-center">
                 <VolverButton url={urlCatalogo}/>
-                <h2 className="col-span-2 text-xl text-right">Categoría: <span className="text-primary">{implemento.categoria}</span></h2>
+                <h2 className="col-span-2 text-xl text-right">Marca: <span className="text-primary">{tractor.marca}</span></h2>
             </div>
 
             {/* Product Image */}
-            <h1 className="text-4xl font-bold mb-2 col-span-2 text-center">{implemento.name}</h1>
+            <h1 className="text-4xl font-bold mb-2 col-span-2 text-center">{tractor.name}</h1>
 
             <div className="flex flex-col items-center col-span-2 md:col-span-1 max-h-90">
                 <img
-                    src={implemento.image}
-                    alt={implemento.name}
+                    src={tractor.image}
+                    alt={tractor.name}
                     className="w-fit object-contain rounded-lg bg-white shadow-sm max-h-90"
                 />
                 {/* (Optional) thumbnails if you had more images */}
@@ -48,9 +48,8 @@ export default function ImplementoPage({ params }: Props){
             <div className="flex flex-col col-span-1">
 
                 <div className="space-y-2 text-gray-700 mb-12 text-lg">
-                    <p><span className="font-semibold">Marca:</span> {implemento.marca}</p>
-                    <p><span className="font-semibold">Modelo:</span> {implemento.modelo}</p>
-                    {/* <p><span className="font-semibold">Categoría:</span> {implemento.categoria}</p> */}
+                    <p><span className="font-semibold">Marca:</span> {tractor.marca}</p>
+                    <p><span className="font-semibold">HP:</span> {tractor.hp}</p>
                 </div>
 
 
@@ -75,7 +74,7 @@ export default function ImplementoPage({ params }: Props){
 
         {/* Extra Section */}
         <section className="max-w-6xl mx-auto px-6 mt-16 col-span-2">
-            <h2 className="text-2xl font-semibold mb-6">Productos relacionados: <span className="text-primary">{implemento.categoria}</span></h2>
+            <h2 className="text-2xl font-semibold mb-6">Productos relacionados: <span className="text-primary">{tractor.marca}</span></h2>
             {/* Grid of related items here */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
                 {recomendados.map(r=>
