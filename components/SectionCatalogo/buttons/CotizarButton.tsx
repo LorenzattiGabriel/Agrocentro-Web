dbc6897994b7d50c3252e2b78f7c334ca7b2c4a5
Rodrigo Quevedo (TalentHub) <rodrigo.quevedo@talenthubai.com.ar>
2025-10-03 18:47:15 -0300

    feature: ficha producto Implemento

--- components/SectionCatalogo/buttons/CotizarButton.tsx
+++ components/SectionCatalogo/buttons/CotizarButton.tsx
@@ -1,24 +1,34 @@
 
 
+const wppButtonId = "WhatsApp-icon-img-contactar-button";
+
 export default function CotizarButton(){
+
+    const toggleRed = ()=>{document.getElementById(wppButtonId)?.classList.add("icon-hover-red")}
+    const disableRed = ()=>{document.getElementById(wppButtonId)?.classList.remove("icon-hover-red")}
 
     return (
         <button
-            //TO DO: mostrar un popup con el formulario para la cotización
+            type="button"
             onClick={()=>{window.open("https://wa.me/#", "_blank", "noopener,noreferrer")}} //noopener,noreferrer: medida de seguridad contra Reverse Tabnabbing
             
-            className="
-                flex items-center gap-2 justify-center
+            className=" group
+                flex items-center gap-3 justify-center
                 w-full py-1.5 px-4
                 rounded
                 font-medium 
-                bg-green-600 text-white 
-                hover:bg-green-900 hover:cursor-pointer
-                transition-colors
-                glow-sweep-right-delay
+                bg-primary text-white 
+                active:bg-background active:text-primary active:border-1 active:border-primary
+                hover:scale-101
+                
+                cursor-pointer
+                transition
+                glow-sweep-right
+                h-full
         ">
-            <img src="/icons/WhatsApp.svg" alt="w" className="invert w-5"/>
-            <span className="text-lg relative -top-[1.5px]">Cotizar</span>
+            <img src="/icons/WhatsApp.svg" alt="w" className="invert w-5 filter group-active:[filter:invert(17%)_sepia(98%)_saturate(7496%)_hue-rotate(1deg)_brightness(85%)_contrast(105%)]" id={wppButtonId}/>
+
+            <span className="text-xl relative -top-[1px]">Cotizar</span>
         </button>
     )
 }
